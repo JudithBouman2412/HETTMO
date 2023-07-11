7fbbab4ff3f7ee5429e4c6619c00e140b0fefed1
JudithBouman2412 <judithabouman@gmail.com>
2023-07-11 09:55:06 +0200

    Remove unneccasery parts

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(...) {
-  cmdstan_version <- cmdstanr::cmdstan_version(error_on_NA = FALSE)
-  if (is.null(cmdstan_version)) {
-    stop("No CmdStan installation found. Run cmdstanr::install_cmdstan() to install.", call. = FALSE)
-  }
-}
